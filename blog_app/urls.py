ad088622960d765f52b1bc69c5829fa97fd5e940
ehsanullah0101 <ehsan.h89@gmail.com>
2021-01-14 16:21:27 -0800

    Adding new version

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('post/<int:pk>/', views.post_detials, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,9 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 # Create your views here.
 
 
 def home(request):
     s = Post.objects.all()
     return render(request, "post_list.html", {"posts": s})
+
+
+def post_detials(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'post_detail.html', {"post": post})
+
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.publish_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'post_new.html', {"form": form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.publish_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, 'post_new.html', {'form': form})
